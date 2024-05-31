f0fb4fe35c612635409ac5d36f4a0d46f746a152
sridharan <sridharan01234@gmail.com>
2024-05-31 13:32:03 +0530

    Initial commit

---
+++ resources/views/laravel-test.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Laravel Test</title>
+    </head>
+    <body>
+        <h1>Laravel Test</h1>
+        <p>This is a test page.</p>
+        <p>This is a test page.</p>
+        <p>This is a test page.</p>
+    </body>
+</html>
